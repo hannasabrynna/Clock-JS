85b0e08a0b86c61cc20ade5df11d99a876b66cc3
hannasabrynna <hannasabrynna135@gmail.com>
2023-11-12 16:25:02 -0300

    Adicionando a data atual

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const getHours = () => {
-    const clock = document.getElementsByClassName('clock')[0]
-    const date = new Date()
+const getTimeDate = () => {
+  const clock = document.getElementsByClassName('clock')[0]
+  const currentDate = document.getElementsByClassName('currentDate')[0]
 
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-    const hour = hours < 10 ? `0${hours}` : hours //quando o número da hora for menor que 10, adciona o 0 na frente;
-    const minute = minutes < 10 ? `0${minutes}` : minutes
-    const second = seconds < 10 ? `0${seconds}` : seconds
+  const date = new Date()
 
-    clock.innerHTML = `${hour}:${minute}:${second}`
-    
-  }
-  
-  setInterval(() => { { getHours()}} , 1000  )
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Os meses começam do zero, então adicionamos 1
+  const year = date.getFullYear();
+  const formattedDate = `${day}/${month}/${year}`;
+
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const hour = hours < 10 ? `0${hours}` : hours //quando o número da hora for menor que 10, adciona o 0 na frente;
+  const minute = minutes < 10 ? `0${minutes}` : minutes
+  const second = seconds < 10 ? `0${seconds}` : seconds
+
+  clock.innerHTML = ` ${hour}:${minute}:${second}`
+  currentDate.innerHTML = ` ${formattedDate}`
+
+}
+
+setInterval(() => { { getTimeDate() } }, 1000)
 
 
-    
-  
+
